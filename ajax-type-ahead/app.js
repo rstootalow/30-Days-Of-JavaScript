06cd0ae60051d2fe8c6b2bb8b2325707d49b7162
rstutalo <richardtutalo@gmail.com>
2019-11-04 22:10:16 -0700

    Logic refactored a bit with fat arrow functions. css styling complete. Logic works for most part but some highlighting on state name needs to be refined.

--- ajax-type-ahead/app.js
+++ ajax-type-ahead/app.js
@@ -41,4 +41,4 @@ const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener(keyup', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
