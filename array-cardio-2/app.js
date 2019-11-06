ce3e387115fbc0ad5e0fef146a48420e5e525726
rstutalo <richardtutalo@gmail.com>
2019-11-05 20:47:05 -0700

    created some functions using some(), every(), and findIndex() array methods.

--- array-cardio-2/app.js
+++ array-cardio-2/app.js
@@ -15,12 +15,67 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+// const isAdult = people.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if(currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
+
+// REFACTOR 1
+// const isAdult = people.some(person => {
+//   const currentYear = (new Date()).getFullYear();
+//   return currentYear - person.year >= 19;
+// })
+
+// REFACTOR 2
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19 );
+console.log('********* Checking For One Adult ********');
+console.log(isAdult);
+
 // Array.prototype.every() // is everyone 19 or older?
+// const allAdults = people.every(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if(currentYear - person.year >= 19)
+//   return true;
+// })
+
+// REFACTOR 1
+// const allAdults = people.every(person => {
+//   const currentYear = (new Date()).getFullYear();
+//   return currentYear - person.year >= 19;
+// });
+
+// REFACTOR 2
+const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+console.log('******** Checking If All Are Adults ********');
+console.log(allAdults)
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+// const comment = comments.find(function(comment) {
+//   if(comment.id === 823423) {
+//     return true;
+//   }
+// });
+
+// FAT ARROW REFACTOR
+const comment = comments.find(comment => {
+  return comment.id === 823423;
+});
+
+console.log('****** Check for Specific ID ******');
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
+const index = comments.findIndex(comment => comment.id === 823423)
+console.log('****** Find Index of ID 823423');
+console.log(index);
 // delete the comment with the ID of 823423
+
+/* SPLICE Method */
+comments.splice(index, 1);
+console.log('****** Table View of New Comments with ID Deleted');
+console.table(comments);
