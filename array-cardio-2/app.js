d70c8a8bb223417c329a37c907d735ace0d4bd01
rstutalo <richardtutalo@gmail.com>
2019-11-05 18:50:34 -0700

    Skeleton html and css complete. Array data in app.js populated as well.

---
+++ array-cardio-2/app.js
@@ -0,0 +1,26 @@
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Cate', year: 1986 },
+  { name: 'Chadwick', year: 1970 },
+  { name: 'Kevin', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good!', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my favorite food ever', id: 123523 },
+  { text: 'Nice Nice Nice', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
