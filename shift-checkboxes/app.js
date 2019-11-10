9bd37f8dda6d0bd5e565ec4616f1cad02fec74a1
rstutalo <richardtutalo@gmail.com>
2019-11-09 23:08:13 -0700

    All javascript coded and logic is working as expected.

---
+++ shift-checkboxes/app.js
@@ -0,0 +1,33 @@
+// select all checkbox inputs
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+// init variable for the last checkbox checked
+let lastChecked;
+
+// function to process if/when a checkbox is lastChecked
+function handleCheck(event) {
+  // variable to identify checkboxes between 1st and 2nd box checked.
+  let inBetween = false;
+  // check if shift key is down when clicking AND check if box is checked
+  if(event.shiftKey && this.checked) {
+    // loop over each checkbox
+    checkboxes.forEach(checkbox => {
+      // console.log(this);
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked ) {
+        inBetween = !inBetween;
+        console.log('Starting to check them in between!')
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+
+}
+
+//event listener for click of checkbox
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
