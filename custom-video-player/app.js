f6cb4e3fb28595439d1d1337fcd1ba5191d0faeb
rstutalo <richardtutalo@gmail.com>
2019-11-11 20:00:03 -0700

    toggleplay function working properly.

--- custom-video-player/app.js
+++ custom-video-player/app.js
@@ -1,8 +1,20 @@
-/* Get elements */
+/* GET HTML ELEMENTS */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressbar = player.querySelector('.progress_filled');
+const progressBar = player.querySelector('progress_filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySlector('data-skip');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player_slider');
+
+
+/* BUILD OUT FUNCTIONS */
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+/* LINK ELEMENTS TO EVENT LISTENERS */
