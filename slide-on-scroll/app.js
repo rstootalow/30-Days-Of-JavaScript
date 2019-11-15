f1d7e44431f5b5df9b7078c38d07758c03f7ec7b
rstutalo <richardtutalo@gmail.com>
2019-11-14 22:49:27 -0700

    app functioning as expected.

---
+++ slide-on-scroll/app.js
@@ -0,0 +1,32 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+    // half way through the image Scroll
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // bottom of image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const ishalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (ishalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
