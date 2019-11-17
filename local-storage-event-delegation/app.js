d4fa18c871cb9161df8f33aef9c4e3c3828c4c00
rstutalo <richardtutalo@gmail.com>
2019-11-17 16:38:10 -0700

    input fields taking user input and the list is populating as expected.

--- local-storage-event-delegation/app.js
+++ local-storage-event-delegation/app.js
@@ -7,3 +7,32 @@ const itemsList = document.querySelector('.plates');
 
 // empty array to store all selected items
 const items = []
+
+
+function addItem(event) {
+  event.preventDefault();
+  // this refers to <form> querySelector grabs any element with name attr of item. Wrap in () as order of operations, querySelector first, then value of input
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+  // console.table(item);
+  items.push(item);
+  populateList(items, itemsList)
+  this.reset(); // form elements have reset() built in to them to clear after submissions.
+}
+
+function populateList(plates = [], platesList) {
+  // loop over array of data and, using .map,  return one large string
+  platesList.innerHTML = plates.map((plate, i) => {
+    // return the html you want to inject into the document
+    return `
+      <li>
+        <label for="">${plate.text}</label>
+      </li>
+    `;
+  }).join('') // map will return an array. .join will join that data as one large string
+}
+
+addItems.addEventListener('submit', addItem);
