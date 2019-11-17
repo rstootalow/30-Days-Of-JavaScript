92d375ce1414d1f0974f797bca3085793e5db13e
rstutalo <richardtutalo@gmail.com>
2019-11-16 17:52:59 -0700

    svg fish image added to html file.

---
+++ local-storage-event-delegation/app.js
@@ -0,0 +1,9 @@
+
+// select form for adding items
+const addItems = document.querySelector('.add-items');
+
+// selector to select items off of menu
+const itemsList = document.querySelector('.plates');
+
+// empty array to store all selected items
+const items = []
