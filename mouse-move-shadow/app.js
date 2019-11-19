409c87618d97c564383c1f581311efbef248dc8a
rstutalo <richardtutalo@gmail.com>
2019-11-18 22:39:48 -0700

    all functionality working as expected.

--- mouse-move-shadow/app.js
+++ mouse-move-shadow/app.js
@@ -1,7 +1,8 @@
 const hero = document.querySelector('.hero');
 const text = document.querySelector('h1');
+const walk = 500; // 100px
 
-function shadow(event) {
+function shadow(e) {
   // console.log(event)
   // const width = hero.offsetWidth;
   // const height = hero.offsetHeight;
@@ -9,13 +10,24 @@ function shadow(event) {
   // ES6 Destructuring
   const { offsetWidth: width, offsetHeight: height } = hero;
   // determine user's cursor location (use let since x,y values may change)
-  let { offsetX : x, offsetY: y } = event; // will also get location of children elements starting at 0,0 which is bad
+  let { offsetX : x, offsetY: y } = e; // will also get location of children elements starting at 0,0 which is bad
 
   /* this code will offset the difference between hero and child element being hovered. */
-  if (this != event.target) {
-    x = x + event.target.offsetLeft;
-    y = y + event.target.offsetTop
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop
   }
+  // console.log(x, y);
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2.5));
+  const yWalk = Math.round((y / height * walk) - (walk / 2.5));
+
+    text.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
 
   // console.log(x, y);
 }
