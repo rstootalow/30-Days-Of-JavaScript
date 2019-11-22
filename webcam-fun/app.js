06b8af44122839ee9035d3ef4862ecf11af16c3c
rstutalo <richardtutalo@gmail.com>
2019-11-21 23:24:28 -0700

    activate webcam video working as expected.

---
+++ webcam-fun/app.js
@@ -0,0 +1,19 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+// get video from Webcam
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+      .catch(err => {
+        console.log('There has been an error', err)
+      });
+}
