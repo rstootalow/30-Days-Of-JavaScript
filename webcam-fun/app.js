a971f3fd83e78553302f907b369cd1c092c2fbfe
rstutalo <richardtutalo@gmail.com>
2019-11-23 14:46:38 -0700

    paintToCanvas function is completed and working as expected.

--- webcam-fun/app.js
+++ webcam-fun/app.js
@@ -18,4 +18,40 @@ function getVideo() {
       });
 }
 
-// getVideo();
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+} // end of paintToCanvas
+
+// function paintToCanvas() {
+//   const width = width.videoWidth;
+//   const height = video.videoHeight;
+//   // console.log(width, height); this call the width and height of the actual video in the browser;
+//
+//   // make sure the canvas is the same saize as the webcam video
+//   canvas.width = width;
+//   canvas.height = height;
+//
+//   setInterval(() => {
+//     ctx.drawImage(video, 0, 0, width, height); // pass in video element, start at top corners(0,0) and run the entire width and height
+//   }, 16)
+// } // end of paintToCanvas
+
+getVideo();
