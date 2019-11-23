bf3afac86b5edab9688e4cd4a830feae874e05a1
rstutalo <richardtutalo@gmail.com>
2019-11-23 13:46:17 -0700

    getVideo function working on page load.

--- webcam-fun/app.js
+++ webcam-fun/app.js
@@ -3,3 +3,19 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// get video
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false }) // this will return a promise
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+      .catch(err => {
+        console.error("Access to camer was denied!". err);
+      });
+}
+
+// getVideo();
