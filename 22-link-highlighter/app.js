1f8e37e0364fa1e3c0f852eb1a07e5b965a3c200
rstutalo <richardtutalo@gmail.com>
2019-11-26 07:16:33 -0700

    all logic working as expected.

--- 22-link-highlighter/app.js
+++ 22-link-highlighter/app.js
@@ -8,10 +8,22 @@ document.body.append(highlight);
 // function to highlight Link
 function highlightLink() {
   // TEST
-  console.log('Highlighting'); // Working
-  console.log('****** Logging This ******');
-  console.log(this);
-  // const linkCoords = this.getBouncingClientRect();
+  // console.log('Highlighting'); // Working
+  // console.log('****** Logging This ******');
+  // console.log(this);
+  const linkCoords = this.getBoundingClientRect()
+  console.log(linkCoords);
+  // coordinate object to align highlight properly
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 // event listener for hovering
