814490f2c357f7325a7058e2fadf832c1d4c7514
rstutalo <richardtutalo@gmail.com>
2019-11-27 17:26:44 -0700

    populateVoices function is working and voices are speaking text when prompted in console.

--- 23-speech-synthesis/app.js
+++ 23-speech-synthesis/app.js
@@ -4,3 +4,16 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+// setting default text on load
+msg.text = document.querySelector('[name="text"]').value;
+
+
+function populateVoices() {
+  // store getVoices into voices array
+  voices = this.getVoices();
+  const voiceOptions = voices
+    .map(voice => `<option value ="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+// using speechSynthesis global variable
+speechSynthesis.addEventListener('voicechanged', populateVoices);
