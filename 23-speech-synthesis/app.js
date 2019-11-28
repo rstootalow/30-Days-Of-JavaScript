e283d3fe97b49a56efbadef5d94e9ec1683f5031
rstutalo <richardtutalo@gmail.com>
2019-11-28 15:17:46 -0700

    toggle working correctly. toggle(false) stops playing toggle() restarts loop

--- 23-speech-synthesis/app.js
+++ 23-speech-synthesis/app.js
@@ -21,6 +21,16 @@ function setVoice() { // to set voice we need to find the correct speechSynthesi
   console.log(this.value);
   msg.voice = voices.find(voice => voice.name === this.value);
 }
+
+// functionj to restart voice playing process once it is done playing
+function toggle(startOver = true) {
+    speechSynthesis.cancel(); // stop voice from speaking.catch
+    if (startOver) {
+      speechSynthesis.speak(msg); // play newly selected voice.
+    }
+}
+
+
 // using speechSynthesis global variable
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 
