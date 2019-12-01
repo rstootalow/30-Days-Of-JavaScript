d50ef3f1490831905ed46cb164e1264c4a95d62e
rstutalo <richardtutalo@gmail.com>
2019-11-30 17:16:29 -0700

    mouse events displaying dropdown menus correctly.

--- 26-stripe-follow-along-dropdown/app.js
+++ 26-stripe-follow-along-dropdown/app.js
@@ -1,19 +1,30 @@
 const triggers = document.querySelectorAll('.cool > li');
 // console.log(triggers);
-
 const background = document.querySelector('.dropdownBackground')
 // console.log(background);
-
 const nav = document.querySelector('.top');
 // console.log(nav);
 
 function handleEnter() {
-  console.log('enter')
+  // console.log('enter')
+  console.log(this);
+  this.classList.add('trigger-enter');
+  // FAT Arrow func will work because `this` inherits value of parent function
+  setTimeout(() => this.classList.add('trigger-enter-active'), 200);
+
+  /* This will not work because when you enter into a function, the value of `this` changes to current function  */
+  // setTimeout(function() {
+  //   this.classList.add('trigger-enter-active');
+  // }, 150)
 }
 
 function handleLeave() {
-  console.log('leave');
+  // console.log('leave');
+  this.classList.remove('trigger-enter');
+  setTimeout(() => this.classList.remove('trigger-enter-active'), 200);
 }
+
+
 
 // mouse event listeners for triggers
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
