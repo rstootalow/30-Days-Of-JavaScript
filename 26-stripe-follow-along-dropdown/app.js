961ce5c67d7c69d988a7be58bdafe87862715ee2
rstutalo <richardtutalo@gmail.com>
2019-12-01 00:50:02 -0700

    refactored handleEnter function a bit with some conditional logic. App working as expcted. Pushing to master.

--- 26-stripe-follow-along-dropdown/app.js
+++ 26-stripe-follow-along-dropdown/app.js
@@ -1,31 +1,54 @@
 const triggers = document.querySelectorAll('.cool > li');
-// console.log(triggers);
-const background = document.querySelector('.dropdownBackground')
-// console.log(background);
+const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
-// console.log(nav);
+
 
 function handleEnter() {
-  // console.log('enter')
-  console.log(this);
+  // console.log(this);
+  // add class to show dropdown menu
   this.classList.add('trigger-enter');
-  // FAT Arrow func will work because `this` inherits value of parent function
-  setTimeout(() => this.classList.add('trigger-enter-active'), 200);
+  // timeout delay for opacity transition with conditional logic to display intended menu
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+  // opacity for background display
+  background.classList.add('open');
 
-  /* This will not work because when you enter into a function, the value of `this` changes to current function  */
-  // setTimeout(function() {
-  //   this.classList.add('trigger-enter-active');
-  // }, 150)
+
+  // getting and setting coordinates and offsets for layout
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  // object to store coords values
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    // nav coords offset to account for possible layout issues
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
+  };
+
+  // apply coord styles to dropdownBackground
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
 }
 
+// handle mouse leave
 function handleLeave() {
-  // console.log('leave');
-  this.classList.remove('trigger-enter');
-  setTimeout(() => this.classList.remove('trigger-enter-active'), 200);
+  // console.log('LEAVE');
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
 }
 
-
-
-// mouse event listeners for triggers
+// Event Listeners for mouse events
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
 triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+
+
+
+/*  SOME NOTES */
+
+/* This will not work because when you enter into a function inside of another function, the value of `this` changes to current function  */
+// setTimeout(function() {
+//   this.classList.add('trigger-enter-active');
+// }, 150)
